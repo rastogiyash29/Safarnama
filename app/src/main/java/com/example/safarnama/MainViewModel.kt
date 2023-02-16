07335fcff76c40daf7339d466ee3dd5c1b6ac149
Yash Rastogi <rastogiyash29@gmail.com>
2023-02-17 00:38:12 +0530

    Second test commit

--- app/src/main/java/com/example/safarnama/MainViewModel.kt
+++ app/src/main/java/com/example/safarnama/MainViewModel.kt
@@ -15,8 +15,6 @@ class MainViewModel(var context:Context):ViewModel() {
         quoteLive.value=getQuote()
     }
 
-    var xyz=1
-
     private fun loadQuoteFromAssets(): ArrayList<Quote> {
         val inputStream = context.assets.open("quotes.json")
         val size: Int = inputStream.available()
